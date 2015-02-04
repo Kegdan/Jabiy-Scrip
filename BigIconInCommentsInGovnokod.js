530aff70e5f8a7341bb35a64b4d14e8c20686247
Kegdan <kegdan@gmail.com>
2015-02-05 00:48:17 +0600

    Better
    and better

--- BigIconInCommentsInGovnokod.js
+++ BigIconInCommentsInGovnokod.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         BigIconInComments
+// @name         BigIconInCommentsInGovnokod
 // @namespace    http://your.homepage/
 // @version      0.1
 // @description  enter something useful
@@ -13,12 +13,22 @@ var bigIconInComments = function(){
 		function() { 
 			var newVal = $(this).attr("src").replace(/([^?])\?.*$/, "$1");
       	    newVal = newVal.replace("28.png","100.png");
-			$(this).attr("src", newVal)
+			$(this).attr("src", newVal);
+            $(this).css("float", "left").css("margin", "0px 10px 0px 0px")
 		}
 	).removeClass("avatar");
-
-	
+    
+    $(".entry-comment-wrapper").each(
+		function() { 
+			var text = $(this).find(".comment-text");
+            var answer = $(this).find(".answer").css("margin", "0px 0px 0px 0px");;
+            text.append("<p></p>").append(answer);
+		}
+	).css("overflow", "auto");
+    
     $(".entry-info").css("margin", "10px");
+    $(".entry-comment").css("margin", "10px 0px 0px 10px");
+    $(".entry-author").css("margin", "0px");
 }
 
 $(document).ajaxComplete(bigIconInComments);
