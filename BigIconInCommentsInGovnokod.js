7c146a2f43430a3e5d2c54f92b9f7a57997f31d6
Kegdan <kegdan@gmail.com>
2015-02-05 02:08:59 +0600

    Some fixes

--- BigIconInCommentsInGovnokod.js
+++ BigIconInCommentsInGovnokod.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BigIconInCommentsInGovnokod
 // @namespace    -
-// @version      1.2
+// @version      1.3
 // @description  Use it, anonimus
 // @author       Kegdan
 // @include http://govnokod.ru/*
@@ -11,15 +11,15 @@
 var bigIconInComments = function(){
 	$(".entry-comments").find(".avatar").each(
 		function() { 
-			var newVal = $(this).attr("src").replace(/([^?])\?.*$/, "$1").replace("28.png","100.png");
-			$(this).attr("src", newVal).css("float", "left").css("margin", "0px 10px 0px 0px").height(80).width(80);
+			var newVal = $(this).attr("src").replace(/\?.*$/, "").replace("28.png","100.png");
+            $(this).attr("src", newVal).css({"float":"left","margin-right":"10px"}).height(80).width(80);
 		}
 	).removeClass("avatar");
     
     $(".entry-comment-wrapper").each(
 		function() { 
-			var text = $(this).find(".entry-comment").css("margin", "10px 0px 0px 10px");
-            var answer = $(this).find(".answer").css("margin", "0px 0px 0px 0px");
+			var text = $(this).find(".entry-comment").css("margin", "10px 0px 0px 0px");
+            var answer = $(this).find(".answer").css("margin", "0px");
             text.append("<p></p>").append(answer);
 		}
 	).css("overflow", "auto");
