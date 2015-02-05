5909c93049aa4f920a7a8d701a961dec37398731
Kegdan <kegdan@gmail.com>
2015-02-05 21:52:28 +0600

    update

--- BigIconInCommentsInGovnokod.js
+++ BigIconInCommentsInGovnokod.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         BigIconInCommentsInGovnokod
+// @name         Govnokod: Big Icon In Comments
 // @namespace    -
 // @version      1.3
 // @description  Use it, anonimus
@@ -12,18 +12,18 @@ var bigIconInComments = function(){
 	$(".entry-comments").find(".avatar").each(
 		function() { 
 			var newVal = $(this).attr("src").replace(/\?.*$/, "").replace("28.png","100.png");
-			$(this).attr("src", newVal).css({"float":"left","margin-right":"10px"}).height(80).width(80);
+            $(this).attr("src", newVal).css({"float":"left","margin-right":"10px"}).height(80).width(80);
 		}
 	).removeClass("avatar");
     
     $(".entry-comment-wrapper").each(
 		function() { 
-			var text = $(this).find(".entry-comment").css("margin", "10px 0px 0px 0px");
-        		var answer = $(this).find(".answer").css("margin", "0px");
-        		text.append("<p></p>").append(answer);
+			var text = $(this).find(".entry-comment").css("margin", "10px 0px 0px 0.5em");
+            var answer = $(this).find(".answer").css("margin", "0px");
+            text.append("<p></p>").append(answer);
 		}
 	).css("overflow", "auto");
-    
+    $(".edit-comment-link").css("margin-left", "0.5em");
     $(".entry-author").css("margin", "0px");
 }
 
