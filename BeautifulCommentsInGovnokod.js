ccd38abc1bc03e7195bffa7d7aa0f9c901de5712
Kegdan <kegdan@gmail.com>
2015-02-05 06:44:00 +0600

    Now with colors!

--- BeautifulCommentsInGovnokod.js
+++ BeautifulCommentsInGovnokod.js
@@ -1,20 +1,28 @@
 // ==UserScript==
 // @name         Beautiful Comments
 // @namespace    -
-// @version      1.0
+// @version      2.0
 // @description  Use it, anonimus!
 // @author       Kegdan
 // @include http://govnokod.ru/*
 // @include http://www.govnokod.ru/*
 // @grant        none
 // ==/UserScript==
+var i = 0;
+var colors = ["#FFCCCC","#FFCC66","#FFFF99","#66FF99","#CCFFFF","#99CCFF","#CC99FF"];
 var BeautifulComments = function(){
 	$("div.entry-comment-hidden").removeClass('entry-comment-hidden');
 
 	$("div.entry-comments ul .hcomment ul").css("margin-left","0.5em");
 
 	$(".hcomment").css({"border-width":"1px 0px 1px 1px",
-                    "border-style":"solid","border-color":"#000000",});
+                    "border-style":"solid","border-color":"#000000"})
+    .each(function (){
+        $(this).css("background-color",colors[i]);
+        if(i == colors.length-1) i=0; else i++;
+    });
+	$("div").css("background-color",$("div").css("background-color"));
+    $(".published").css("color","#000000");
 };
 
 $(document).ajaxComplete(BeautifulComments);
