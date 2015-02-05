f194b68dba2da33d39121f2667d688d0f1b793bd
Kegdan <kegdan@gmail.com>
2015-02-05 06:00:36 +0600

    Version 1.1

--- BeautifulCommentsInGovnokod.js
+++ BeautifulCommentsInGovnokod.js
@@ -8,10 +8,15 @@
 // @include http://www.govnokod.ru/*
 // @grant        none
 // ==/UserScript==
+var BeautifulComments = function(){
+	$("div.entry-comment-hidden").removeClass('entry-comment-hidden');
 
-$("div.entry-comment-hidden").removeClass('entry-comment-hidden');
+	$("div.entry-comments ul .hcomment ul").css("margin-left","0.5em");
 
-$("div.entry-comments ul .hcomment ul").css("margin-left","0.5em");
-
-$(".hcomment").css({"border-width":"1px 0px 1px 1px",
+	$(".hcomment").css({"border-width":"1px 0px 1px 1px",
                     "border-style":"solid","border-color":"#000000",});
+};
+
+$(document).ajaxComplete(BeautifulComments);
+BeautifulComments();
+
