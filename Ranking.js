ae9501237e0f13c2b99f6880b3c0f5cddfb3b527
Kegdan <kegdan@gmail.com>
2015-05-07 19:17:48 +0600

    Bag fixed
    True story

--- Ranking.js
+++ Ranking.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Govnokod: Ranking
 // @namespace    -
-// @version      1.0
+// @version      1.1
 // @description  Use it, anonimus!
 // @author       Kegdan
 // @include http://govnokod.ru/*
@@ -12,4 +12,4 @@
 var ids = $('.entry-author a').map(function(_,x){return x.href.replace(/.*\/(\d+)/,'$1')*1});
 var max = Math.max.apply(Math,ids);
 var min = Math.min.apply(Math,ids);
-$('.entry-author a').each(function(i,x){x.style.color = 'rgb(0,'+Math.floor(((ids[i] - min)*255)/max)+', 0)'})
+$('.entry-author a').each(function(i,x){x.style.color = 'rgb(0,'+Math.floor(((ids[i] - min)*255)/(max-min))+', 0)'})
